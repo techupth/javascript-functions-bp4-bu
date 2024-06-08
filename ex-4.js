aa5963605d78a4a7ce0566253bbd34761555602d
Bura <bu_bangplee@hotmail.com>
2024-06-08 16:19:17 +0700

    feat: submit JavaScript Functions Assignment

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,21 @@
 // Start coding here
-let calculator = {};
+// Start coding here
+let add = (a,b) => {
+    return a+b;
+}
+let subtract = (a,b) => {
+    return a-b;
+}
+let multiply = (a,b) => {
+    return a*b;
+}
+let divide = (a,b) => {
+    return a/b;
+}
+
+let calculator = {add,subtract,multiply,divide};
+
+let addResult = calculator.add(10,20);
+console.log(addResult);
+let divideResult = calculator.divide(3000,10);
+console.log(divideResult);
